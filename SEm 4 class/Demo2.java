80b3f41380714c92bcfec267e1b8db784dd541f8
raiayush2610 <ayushayushravi@gmail.com>
2023-06-05 13:07:14 +0530

    my-third-commit

--- SEm 4 class/Demo2.java
+++ SEm 4 class/Demo2.java
@@ -5,7 +5,7 @@ class A{
 class CSE extends Thread {
     public void run(){
         for (int i = 0; i < 5; i++) {
-            System.out.println("I am in class");
+            System.out.println("I am at class");
             try {Thread.sleep(1000);} catch (Exception e) {}
      }
 }
