1996ae92202c43db00ff3953741b0a7d318eaa6d
raiayush2610 <ayushayushravi@gmail.com>
2023-09-02 13:25:32 +0530

    comment

--- LINKed listr/Link2.java
+++ LINKed listr/Link2.java
@@ -1,14 +1,15 @@
 import java.util.*;
+public class Link2 {
 public static class Node{
         int data;
         Node next;
         public Node(int data){
             this.data= data;
             this.next =null;
-
+// this is comment
         }
     }
-public class Link2 {
+
     public static void main(String[] args) {
         LinkedList<Integer> ll  =new LinkedList <>();
 
